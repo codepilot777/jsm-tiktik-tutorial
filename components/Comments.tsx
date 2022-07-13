8cabe53464bfb0451f8ffdec95bb65f2d25f0e36
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-07-13 13:23:00 +0800

    complete comment page

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import {GoVerified} from 'react-icons/go'
 
 import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
+import { IUser } from '../types';
 
 interface IProps {
   comment: string,
@@ -26,15 +27,45 @@ interface IComment {
 
 const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
 
-  const {userProfile} = useAuthStore();
+  const {userProfile, allUsers} = useAuthStore();
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
         {
           comments?.length ? (
-            <div>
-              videos
-            </div>
+            comments.map((item, idx) => (
+              <>
+                {allUsers?.map((user: IUser) => user._id === (item.postedBy._id || item.postedBy._ref) && (
+                  <div className="items-center p-2" key={idx}>
+                    <Link href={`/profile/${user._id}`}>
+                      <div className="flex items-start gap-3 cursor-pointer">
+                        <div className="w-8 h-8">
+                          <Image 
+                          src={user.image}
+                          width={34}
+                          height={34}
+                          className="rounded-full"
+                          alt="user profile"
+                          layout="responsive" />
+                        </div>
+                        <div className="hidden xl:block">
+                          <p className="flex items-center gap-1 font-bold lowercase text-md text-primary">
+                            {user.userName.replace(' ','')}
+                            <GoVerified className="text-blue-400" />
+                          </p>
+                          <p className="text-xs text-gray-400 capitalize">
+                            {user.userName}
+                          </p>
+                        </div>
+                      </div>
+                    </Link>
+                    <div>
+                      <p>{ item.comment }</p>
+                    </div>
+                  </div>
+                ))}
+              </>
+            ))
           ) : (
             <NoResults text="No Comments Yet" />
           )
