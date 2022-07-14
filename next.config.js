226954aef28ee9e849416c658c45c610e70aa85a
Lok Yiu Yeung <ryanyeung@Loks-MacBook-Air.local>
2022-07-14 17:14:27 +0800

    change next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript : {
+    ignoreBuildErros: true,
+  },
   reactStrictMode: true,
   images:{
     domains: ['blog.thecodingday.com', 'lh3.googleusercontent.com']
